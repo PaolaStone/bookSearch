a07d4a71eadac6dcf3cedd6a2c2afc8a14e9e964
Paola Stone <paola.stonemartinez@dell.com>
2019-11-17 23:19:50 -0500

    search display working

--- src/Components/BookList/index.js
+++ src/Components/BookList/index.js
@@ -1,21 +1,35 @@
 import React from 'react';
-// import BookCard from './Components/BookCard/index.js'
+// import BookCard from './Components/BookCard'
 
 const BookList = (props) => {
     return(
         <div className="list">
             {
                 props.books.map((book,i) => {
-                    return<div
+                    return<BookCard
                             key={i}        
                             image={book.volumeInfo.imageLinks.thumbnail}
                             tittle={book.volumeInfo.title}
                             author={book.volumeInfo.authors}
                             description={book.volumeInfo.description}
                             link={book.volumeInfo.selfLink}
-                        />
+                            />
                 })
             }
+
+        </div>
+    )
+}
+const BookCard = (props) => {
+    return(
+        <div className="card-container">
+            <img src={props.image} alt=""/>
+            <div className="desc">
+                <h2>{props.title}</h2>
+                <h3>{props.author}</h3>
+                <p>{props.description}</p>
+                <p>{props.link}</p>
+            </div>
 
         </div>
     )
